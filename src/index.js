07f1b0fcd5835179cf980d917ae6b56e4160b168
eugenemoro <29465704+eugenemoro@users.noreply.github.com>
2024-12-08 19:06:46 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import { HomeContent } from './home.js';
 import { MenuContent } from './menu.js';
 import { AboutContent } from './about.js';
 
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
+}
+
 const homeBtn = document.getElementById('nav-home');
 const menuBtn = document.getElementById('nav-menu');
 const aboutBtn = document.getElementById('nav-about');
